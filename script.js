631c2081fd0d7646091becb05d93eea20aeda0c4
Wijepala N.G.N.D <151638928+NirupamaDulanjalee@users.noreply.github.com>
2023-12-14 09:51:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let currentValue = '';
+
+function appendValue(value) {
+  currentValue += value;
+  document.getElementById('display').value = currentValue;
+}
+
+function calculate() {
+  try {
+    const result = eval(currentValue);
+    document.getElementById('display').value = result;
+    currentValue = result.toString();
+  } catch (error) {
+    document.getElementById('display').value = 'Error';
+    currentValue = '';
+  }
+}
+
+function clearDisplay() {
+  document.getElementById('display').value = '';
+  currentValue = '';
+}
